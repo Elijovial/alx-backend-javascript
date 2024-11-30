b81d90ebfba2f14c50e3a6b9c17a8e9b9fdb7d5f
Elijovial <omeehelijah345@gmail.com>
2024-11-30 16:35:38 +0000

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write('Your name is: ' + chunk);
-    }
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('data', (data) => {
+	const name = data.toString().trim();
+	process.stdout.write(`Your name is: ${name}\n`);
+	process.exit();
 });
 
-process.stdin.on('end', function() {
-    process.stdout.write('This important software is now closing\n')
-})
+process.on('exit', () => {
+	console.log("This important software is now closing");
+});
